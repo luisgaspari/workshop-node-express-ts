0dd9fc125e70778cf9302ce963ae6adb6d24ce25
Luis Gaspari <luisgaspari@gmail.com>
2024-03-15 21:28:32 -0300

    Update server port and console logs

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,19 @@ import cors from 'cors'
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
+
+// app.use(
+//     cors({
+//         origin: ['http://localhost:3000', 'https://meuapp.com'],
+//     })
+// )
 
 app.use(cors()) // habilita o cors
 app.use(express.json()) // habilita o express para receber dados no formato json
 app.use(routes) // habilita as rotas
 
 app.listen(port, () => {
-  console.log(`Servidor executando na porta ${port}`)
-  console.log(`Banco de dados`, dataBase.isInitialized ? 'inicializado' : 'não inicializado')
+    console.log(`Servidor executando na porta ${port}`)
+    console.log(`Banco de dados`, dataBase.isInitialized ? 'inicializado' : 'não inicializado')
 })
